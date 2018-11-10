0125760b260678a4b2d994bc51e1846a38a4e3af
语默 <smartwsg@163.com>
2018-11-10 10:58:59 +0800

    公共查询接口格式修改

--- src/api/articleList.js
+++ src/api/articleList.js
@@ -16,23 +16,35 @@ function axiosConfig(config){
 // *****************************主页********************************************
 // 主页轮播图
 export  function getIndexLunbo(config){
-  const url=URLROUTER+'/app/pushPosition/advertisingRotation'
-  return axiosConfig(Object.assign({url},config))
+  let Obj = {
+    'url':URLROUTER+'/app/pushPosition/advertisingRotation',
+    'method':"GET"
+  }
+  return axiosConfig(Object.assign({},Obj,config))
 }
 //g广告分页查询接口
 export  function getadvert(config){
-  const url=URLROUTER+'/app/article/advertisement'
-  return axiosConfig(Object.assign({url},config))
+  let Obj = {
+    'url':URLROUTER+'/app/article/advertisement',
+    'method':"GET"
+  }
+  return axiosConfig(Object.assign({},Obj,config))
 }
 //文章内容分页查询
 export  function getArticleList(config){
-  const url=URLROUTER+'/app/article/advertisement'
-  return axiosConfig(Object.assign({url},config))
+  let Obj = {
+    'url':URLROUTER+'/app/article/advertisement',
+    'method':"GET"
+  }
+  return axiosConfig(Object.assign({},Obj,config))
 }
 //文章详情页面查询
 export  function getArticleDetail(config){
-  const url=URLROUTER+'/app/article/findById';
-  return axiosConfig(Object.assign({url},config))
+  let Obj = {
+    'url':URLROUTER+'/app/article/findById',
+    'method':"GET"
+  }
+  return axiosConfig(Object.assign({},Obj,config))
 }
 
 // 多并发axios请求
