292c09bde731ce305b3621a21f695f358efc0015
wsh <wushaohuai1314@163.com>
2018-12-22 15:57:46 +0800

    发布评论

--- src/components/page/changeCar/carSeriesComment/carSeriesComment.vue
+++ src/components/page/changeCar/carSeriesComment/carSeriesComment.vue
@@ -26,7 +26,7 @@
       <div class="carSeries" @click="chooseDealerHide(item.id,item.dealerName)" v-for="(item,index) in dealerData">
           {{item.dealerName}}
       </div>
-      <div class="carSeries" @click="chooseDealerHide(id='',dealerName='')">其它</div>
+      <div class="carSeries" @click="chooseDealerHide(id='',dealerName='其它')">其它</div>
     </modal>
     <modal ref="mychildFour">
       <div class="changeCar">购车目的  <span @click="buyGoalHide">完成</span></div>
@@ -45,135 +45,46 @@
   </mt-datetime-picker>
     <div class="inputStyle">
       <div @click="chooseCarShow">
-        <span>购买车型 ：</span><input type="text" v-model="carArrayList.modelName" placeholder="选择车型>">
+        <span>购买车型 ：</span><input type="text" ref='valid-modelName' title='请输入购买车型'  v-model="carArrayList.modelName" placeholder="选择车型>">
       </div>
       <div>
-        <span>裸车价格 ：</span><input  type="text" v-model="carArrayList.emptyCar" ><span class="marginRight">万元</span>
+        <span>裸车价格 ：</span><input  type="text" ref='valid-price' title='请输入裸车价格' v-model="carArrayList.price" ><span class="marginRight">万元</span>
       </div>
       <div>
-        <span>行驶里程 ：</span><input  type="text" v-model="carArrayList.travlledDistance"><span class="marginRight">万公里</span>
+        <span>行驶里程 ：</span><input  type="text" ref='valid-mileage' title='请输入行驶里程' v-model="carArrayList.mileage"><span class="marginRight">万公里</span>
       </div>
       <div @click="buyTime">
-        <span>购买时间 ：</span><input type="text" v-model="carArrayList.buyTime" placeholder="请选择日期>">
+        <span>购买时间 ：</span><input type="text" ref='valid-buyDate' title='请选择日期' v-model="carArrayList.buyDate" placeholder="请选择日期>">
       </div>
       <div @click="chooseCityShow()">
-        <span>购买地点 ：</span><input type="text" v-model="carArrayList.contactsRegion" placeholder="请选择城市>">
+        <span>购买地点 ：</span><input type="text" ref='valid-buyAddress' title='请选择城市' v-model="carArrayList.buyAddress" placeholder="请选择城市>">
       </div>
       <div @click="chooseDealerShow">
-        <span>购买经销商 ：</span><input type="text" v-model="carArrayList.dealerName" placeholder="请选择经销商>">
+        <span>购买经销商 ：</span><input type="text" ref='valid-buyDealer' title='请选择经销商' v-model="carArrayList.buyDealer" placeholder="请选择经销商>">
       </div>
       <div>
-        <span>使用油耗 ：</span><input  type="text" v-model="carArrayList.useOil" ><span class="marginRight">L/百公里</span>
+        <span>使用油耗 ：</span><input  type="text" ref='valid-useOil' title='请输入油耗' v-model="carArrayList.useOil" ><span class="marginRight">L/百公里</span>
       </div>
       <div @click="buyGoalShow">
-        <span>购车目的 ：</span><input type="text" v-model="carArrayList.buyPurpose" placeholder="请选择购车目的>">
+        <span>购车目的 ：</span><input type="text" ref='valid-buyPurpose' title='请选择购车目的' v-model="carArrayList.buyPurpose"  placeholder="请选择购车目的>">
       </div>
     </div>
-    <div class="marginBottom">
-      <div class="titleInfo">旧车信息</div>
-      <div class="infoList">
-          <div>
-            <div>*最满意</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
+    <div class="marginBottom"  v-for="item of commentList">    
+      <div class="titleInfo">{{item.title}}</div>
+      <div class="infoList" v-for="comment of item.list">
+          <div class="infoListOne">
+            <div class="commentTitle">{{comment.commentTitle}}</div>
+            <input class="commentInput"  :ref='"valid-"+comment.star.slice(0,-3)+"valid"' :title='comment.commentTitle' v-model="carArrayList[comment.star.slice(0,-3)]" :placeholder="comment.commentInput" />
           </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*最不满意</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
+          <div class="infoListTwo">
+              <star @starMarkChange="markChange" 
+                    :outIndex="comment.star" :ref='"valid-"+comment.star' :title='comment.commentTitle'>
+              </star>
           </div>
       </div> 
-      <div class="infoList">
-          <div>
-            <div>*其他补充</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="titleInfo">给车评分和描述</div>
-      <div class="infoList">
-          <div>
-            <div>*空间(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*动力(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-         <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*操控(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-         <div>
-              <star></star>
-          </div>
-      </div>
-      <div class="infoList">
-          <div>
-            <div>*油耗(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*舒适性(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*外观(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*内饰(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div> 
-      <div class="infoList">
-          <div>
-            <div>*性价比(必选)</div>
-            <div>用车过程有什么满意地方(30字以内)</div>
-          </div>
-          <div>
-              <star></star>
-          </div>
-      </div>
     </div>   
     <div class="issue">
-      <button>发布点评</button>
+      <button @click="submit()">发布点评</button>
     </div>
   </div>
 </template>
@@ -182,10 +93,28 @@
 import contentHeader from '@/common/view/contentHeader';
 import star from '@/common/view/star';
 import modal from '@/common/view/modal';
+import {validForm} from '@/common/js/utils.js';
 import {getFindAllProvince,findCitysByRroId,getCarModelListBySeries,defaultAppList,postBuyCarIntention,appList} from '@/api/changeCar/index';
 export default {
   data() {
     return {
+      commentList:[
+        {title:"旧车信息",list:[
+           {commentTitle:"*最满意:",commentInput:"用车过程有什么满意的地方(30字以内)",star:'satisfyNum'},
+           {commentTitle:"*最不满意:",commentInput:"用车过程有什么不满意的地方(30字以内)",star:'unsatisfyNum'},
+           {commentTitle:"其他补充:", commentInput:"其他补充",star:'otherNum'},
+        ]},
+        {title:"给车评分和描述",list:[
+           {commentTitle:"*空间(必选):",commentInput:"后备箱空间怎么样",star:'spaceNum'},
+           {commentTitle:"*动力(必选):",commentInput:"动力怎么样",star:'powerNum'},
+           {commentTitle:"*操控(必选):",commentInput:"操控怎么样",star:'manipulationNum'},
+           {commentTitle:"*油耗(必选):",commentInput:"油耗怎么样",star:'oliNum '},
+           {commentTitle:"*舒适性(必选):",commentInput:"舒适性怎么样",star:'comfortableNum'},
+           {commentTitle:"*外观(必选):",commentInput:"外观怎么样",star:'appearanceNum'},
+           {commentTitle:"*内饰(必选):",commentInput:"内饰怎么样",star:'trimNum'},
+           {commentTitle:"*性价比(必选):",commentInput:"性价比怎么样",star:'costPerformanceNum'},
+        ]},
+      ],
       provideJudge:true,
       cityJudge:false,
       provideCity:'',
@@ -196,16 +125,38 @@ export default {
       pickerValue:'',
       cityCode:'',
       carArrayList:{
+        satisfy:'',
+        satisfyNum:'',
+        unsatisfy:'',
+        unsatisfyNum:'',
+        other:'',
+        otherNum:'',
+        space:'',
+        spaceNum:'',
+        power:'',
+        powerNum:'',
+        manipulation:'',
+        manipulationNum:'',
+        oli:'',
+        oliNum:'',
+        comfortable:'',
+        comfortableNum:'',
+        appearance:'',
+        appearanceNum:'',
+        trim:'',
+        trimNum:'',
+        costPerformance:'',
+        costPerformanceNum:'',
         modelName:'',
         modelId:'',
         brandCode:'',
         seriesCode:'',
-        emptyCar: "",
-        travlledDistance: "",
-        buyTime: "",
-        contactsRegion:'',
+        price: "",
+        mileage: "",
+        buyDate: "",
+        buyAddress:'',
         dealerId: "",
-        dealerName:'',
+        buyDealer:'',
         useOil: "",
         buyPurpose: "",
       },
@@ -218,6 +169,13 @@ export default {
       });
   },
   methods: {
+    submit(){
+      validForm(this)
+    },
+    // 点击星星触发事件
+    markChange(code,key){
+      this.carArrayList[key] = code
+    },
     chooseCarShow(){
       // 查询车系
       getCarModelListBySeries({
@@ -252,7 +210,7 @@ export default {
     },
     // 点击城市的显示传值
     clickShow(val,code){
-      this.carArrayList.contactsRegion=this.provideCity+'-'+val;
+      this.carArrayList.buyAddress=this.provideCity+'-'+val;
       this.provideCity='';
       this.cityJudge=false;
       this.$refs.mychildTwo.modalHide();
@@ -266,7 +224,7 @@ export default {
     // 购买经销商
     chooseDealerHide(id,dealerName){
       // this.changeCarList=true;
-      this.carArrayList.dealerName=dealerName
+      this.carArrayList.buyDealer=dealerName
       this.carArrayList.dealerId=id
       this.$refs.mychildThree.modalHide();
     },
@@ -275,7 +233,7 @@ export default {
         this.$toast('请选择车型')
         return
       }
-      if(this.carArrayList.contactsRegion==''){
+      if(this.carArrayList.buyAddress==''){
         this.$toast('请选择城市')
         return
       }
@@ -290,6 +248,7 @@ export default {
     },
     buyGoalHide(){
        this.$refs.mychildFour.modalHide();
+       this.carArrayList.buyPurpose="aaa"
     },
     // // 控制购买地点的显示隐藏
     // chooseProvideHide(value){
@@ -311,7 +270,7 @@ export default {
     },
     // 购买时间赋值
     handleConfirm(value){
-      this.carArrayList.buyTime=new Date(value).format("yyyy-MM-dd")
+      this.carArrayList.buyDate=new Date(value).format("yyyy-MM-dd")
     }
   },
   components: {
@@ -403,7 +362,7 @@ export default {
       outline none 
       border none
     .marginBottom
-      margin-bottom 3rem
+      // margin-bottom 3rem
       .titleInfo
         text-align left
         padding 1rem 0.5rem
@@ -418,6 +377,14 @@ export default {
         border-bottom 2px solid #DDDDDD
         &:last-child
           border-bottom none
+        .infoListOne
+          flex-grow 1 
+          input 
+            width 95%
+            font-size 0.5rem
+            border none
+            outline none
+              
 </style>
 
 
